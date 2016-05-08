e4a0c0e917eb553f83f2ce5c8d4da3f8e5505778
Gareth Flowers <garethflowers@users.noreply.github.com>
2016-05-08 22:24:47 +0100

    Create vbox.sh

---
+++ linux/fedora-23/vbox.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cat << EOF > /etc/yum.repos.d/oracle-virtualbox.repo
+[virtualbox]
+name=Fedora \$releasever - \$basearch - VirtualBox
+baseurl=http://download.virtualbox.org/virtualbox/rpm/fedora/\$releasever/\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://www.virtualbox.org/download/oracle_vbox.asc
+EOF
+
+dnf install -y virtualbox dkms
