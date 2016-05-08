efc592dc33f16d3c4cda730c6e319c0845ddea15
Gareth Flowers <garethflowers@users.noreply.github.com>
2016-05-08 22:25:15 +0100

    Create chrome.sh

---
+++ linux/fedora-23/chrome.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cat << EOF > /etc/yum.repos.d/google-chrome.repo
+[google-chrome]
+name=google-chrome - \$basearch
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
+EOF
+
+dnf install -y google-chrome-stable
