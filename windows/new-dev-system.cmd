97008264b86ab985533df2c5d5f0316550f315ca
Gareth Flowers <garethflowers@users.noreply.github.com>
2016-05-11 09:26:01 +0100

    Update new-dev-system.cmd

--- windows/new-dev-system.cmd
+++ windows/new-dev-system.cmd
@@ -1,20 +1,18 @@
+@echo off
+
 :: install chocolatey
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "(iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))) >$null 2>&1" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco feature enable -n allowGlobalConfirmation
 
 :: general apps
-choco install 7zip
-choco install googlechrome
-choco install filezilla
-choco install atom
-choco install putty
+choco upgrade chocolatey 7zip 7zip.commandline putty filezilla adobereader slack javaruntime
+choco upgrade autoruns sdelete procexp procmon
+choco upgrade curl wget wput zip unzip optipng
 
 :: dev apps
-choco install tortoisesvn
-choco install visualstudio2012professional
-choco install ankhsvn
-choco install wixtoolset
-choco install ilspy
-choco install dotnet4.5
-choco install dotnet3.5
-choco install dotnet4.6
+choco upgrade postgresql-9.3 pgadmin3
+choco upgrade tortoisesvn git gitkraken
+choco upgrade atom visualstudiocode
+choco upgrade visualstudio2012professional -packageParameters "/Features:'OfficeTools'"
+choco upgrade ankhsvn wixtoolset ilspy depends
+choco upgrade dotnet4.6 dotnet4.5.2 dotnet4.5.1 dotnet4.5 dotnet3.5
