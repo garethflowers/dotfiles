a2205528635fdccf69b6272f98f6b4da124801fc
Gareth Flowers <garethflowers@users.noreply.github.com>
2016-05-18 21:30:37 +0100

    Add vlc and pencil

--- windows/new-dev-system.cmd
+++ windows/new-dev-system.cmd
@@ -6,15 +6,16 @@ choco feature enable -n allowGlobalConfirmation
 
 :: general apps
 choco upgrade chocolatey
-choco upgrade 7zip 7zip.commandline putty filezilla adobereader slack javaruntime gimp paint.net
+choco upgrade 7zip 7zip.commandline adobereader gimp javaruntime paint.net pencil slack vlc
+choco upgrade filezilla putty
 choco upgrade autoruns procexp procmon sdelete
 choco upgrade curl optipng rsync unzip wget wput zip
 choco upgrade inconsolata
 
 :: dev apps
-choco upgrade postgresql-9.3 pgadmin3
-choco upgrade tortoisesvn git gitkraken
+choco upgrade pgadmin3 postgresql-9.3
+choco upgrade ankhsvn git gitkraken tortoisesvn
 choco upgrade atom visualstudiocode
 choco upgrade visualstudio2012professional -packageParameters "/Features:'OfficeTools'"
-choco upgrade ankhsvn wixtoolset ilspy depends
+choco upgrade depends ilspy wixtoolset
 choco upgrade dotnet4.6 dotnet4.5.2 dotnet4.5.1 dotnet4.5 dotnet3.5
