9db736b2cfe702f8c83f1ebfa7d0b0ba8c5d7595
Gareth Flowers <garethflowers@users.noreply.github.com>
2016-07-22 22:53:19 +0100

    Update new-dev-system.cmd

--- windows/new-dev-system.cmd
+++ windows/new-dev-system.cmd
@@ -3,19 +3,35 @@
 :: install chocolatey
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "(iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))) >$null 2>&1" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco feature enable -n allowGlobalConfirmation
+choco upgrade chocolatey
 
 :: general apps
-choco upgrade chocolatey
-choco upgrade 7zip 7zip.commandline adobereader gimp javaruntime paint.net pencil slack vlc
+choco upgrade 7zip 7zip.commandline adobereader gimp inkscape javaruntime jdk paint.net pencil slack vlc
+
+:: remote
 choco upgrade filezilla putty
-choco upgrade autoruns procexp procmon sdelete
+
+::utils
+choco upgrade autoruns procexp procmon sdelete grepwin
+
+:: command line tools
 choco upgrade curl optipng rsync unzip wget wput zip
+
+:: fonts
 choco upgrade inconsolata
 
 :: dev apps
 choco upgrade pgadmin3
-choco upgrade ankhsvn git gitkraken tortoisesvn
 choco upgrade atom visualstudiocode
+
+:: vcs
+choco upgrade ankhsvn git gitkraken
+choco upgrade tortoisesvn --version 1.9.2
+
+:: dotnet development
 choco upgrade visualstudio2012professional -packageParameters "/Features:'OfficeTools'"
 choco upgrade depends ilmerge ilspy wixtoolset
-choco upgrade dotnet4.6 dotnet4.5.2 dotnet4.5.1 dotnet4.5 dotnet3.5
+choco upgrade dotnet4.6.1 dotnet4.6 dotnet4.5.2 dotnet4.5.1 dotnet4.5 dotnet3.5
+
+:: android development
+choco upgrade android-sdk androidstudio
