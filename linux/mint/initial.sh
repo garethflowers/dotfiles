b0ed63975cc436563b4d4c14d157129eb3f4e7ee
Gareth Flowers <garethflowers@users.noreply.github.com>
2016-10-26 09:28:47 +0100

    Update initial.sh

--- linux/mint/initial.sh
+++ linux/mint/initial.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-apt-get install -y subversion pgadmin3 fonts-inconsolata
+# development
+apt-get install -y subversion pgadmin3
 
+# fonts
+apt-get install -y fonts-inconsolata msttcorefonts
+
+# remote
 apt-get install -y remmina remmina-plugin-rdp remmina-plugin-vnc
