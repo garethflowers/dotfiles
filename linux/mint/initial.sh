a8d6498e3037f941765ec46e36d15953d89c2e19
Gareth Flowers <garethflowers@users.noreply.github.com>
2016-11-13 21:48:37 +0000

    Adds docker

--- linux/mint/initial.sh
+++ linux/mint/initial.sh
@@ -14,3 +14,6 @@ apt-get install -y fonts-droid-fallback fonts-inconsolata msttcorefonts
 
 # remote
 apt-get install -y remmina remmina-plugin-rdp remmina-plugin-vnc
+
+# docker
+wget -qO- https://get.docker.com/ | sh
