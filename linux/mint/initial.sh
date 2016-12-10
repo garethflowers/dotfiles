15c5dcd303337a6fa2ef1ada757ab0c408ff09be
Gareth Flowers <garethflowers@users.noreply.github.com>
2016-12-10 16:05:37 +0000

    Force all apt commands through sudo

--- linux/mint/initial.sh
+++ linux/mint/initial.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 # general
-apt-get install dia gimp inkscape
+sudo apt-get install -y dia gimp inkscape
 
 #browsing
-apt-get chromium-browser torbrowser-launcher
+sudo apt-get install -y chromium-browser torbrowser-launcher
 
 # development
-apt-get install -y subversion pgadmin3
+sudo apt-get install -y subversion pgadmin3
 
 # fonts
-apt-get install -y fonts-droid-fallback fonts-inconsolata msttcorefonts
+sudo apt-get install -y fonts-droid-fallback fonts-inconsolata msttcorefonts
 
 # remote
-apt-get install -y remmina remmina-plugin-rdp remmina-plugin-vnc
+sudo apt-get install -y remmina remmina-plugin-rdp remmina-plugin-vnc
 
 # docker
-wget -qO- https://get.docker.com/ | sh
+sudo wget -qO- https://get.docker.com/ | sh
 sudo usermod -aG docker "$USER"
