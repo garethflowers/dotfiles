f9489290fb4d39aeaeb2008ebb213dba53e72e72
Gareth Flowers <garethflowers@users.noreply.github.com>
2017-02-15 22:13:26 +0000

    Updates new dev system apps

--- windows/new-dev-system.cmd
+++ windows/new-dev-system.cmd
@@ -3,19 +3,21 @@
 :: install chocolatey
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command "(iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))) >$null 2>&1" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco feature enable -n allowGlobalConfirmation
+choco feature enable -n allowEmptyChecksums
 choco upgrade chocolatey
 
 :: general apps
 choco upgrade 7zip adobereader googlechrome slack
 
 :: graphics/diagramming
-choco upgrade dia gimp inkscape paint.net pencil
+choco upgrade dia gimp inkscape paint.net
+choco upgrade pencil -pre
 
 :: media
 choco upgrade vlc
 
 :: remote/networking
-choco upgrade filezilla putty teamviewer tightvnc transmission-qt
+choco upgrade filezilla putty.install teamviewer tightvnc transmission-qt
 
 ::utils
 choco upgrade autoruns procexp procmon grepwin
@@ -29,10 +31,12 @@ choco upgrade inconsolata
 :: dev apps
 choco upgrade pgadmin3
 choco upgrade atom visualstudiocode
+choco upgrade postman
 
 :: vcs
-choco upgrade ankhsvn git gitkraken
-choco upgrade tortoisesvn --version 1.9.2
+choco upgrade git -params '"/GitAndUnixToolsOnPath /NoAutoCrlf /NoShellIntegration /WindowsTerminal"'
+choco upgrdae github gitkraken
+choco upgrade ankhsvn tortoisesvn
 
 :: dotnet development
 choco upgrade visualstudio2012professional -packageParameters "/Features:'OfficeTools WebTools'"
