4e9faf2859dd97568389e91ac17efbb2a75ef54c
Gareth Flowers <garethflowers@users.noreply.github.com>
2017-11-27 11:56:38 +0000

    Adds comments for each group

--- linux/initial.deb.sh
+++ linux/initial.deb.sh
@@ -10,39 +10,48 @@ sudo apt install -y curl libcurl3 wget wput
 sudo apt install -y chromium-browser dia filezilla gimp inkscape torbrowser-launcher vlc
 
 curl --output /tmp/slack.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-2.3.3-amd64.deb
-sudo dpkg --install /tmp/slack.deb
+sudo dpkg -i /tmp/slack.deb
 rm -f /tmp/slack.deb
 
-# development
+# vcs
 sudo apt install -y git subversion
+
+# node
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt install -y nodejs
+
+# php
 sudo apt install -y composer php
+
+# python
 sudo apt install -y python python-pip
 
-curl --output /tmp/atom.deb --location https://atom.io/download/deb
-sudo dpkg --install /tmp/atom.deb
+# atom
+curl -L -o /tmp/atom.deb https://atom.io/download/deb
+sudo dpkg -i /tmp/atom.deb
 sudo apt install -fy
 rm -f /tmp/atom.deb
 
-curl --output /tmp/vscode.deb --location https://go.microsoft.com/fwlink/?LinkID=760868
-sudo dpkg --install /tmp/vscode.deb
+# vscode
+curl -L -o /tmp/vscode.deb https://go.microsoft.com/fwlink/?LinkID=760868
+sudo dpkg -i /tmp/vscode.deb
 rm -f /tmp/vscode.deb
 
-curl --output /tmp/insomnia.deb --location https://builds.insomnia.rest/downloads/ubuntu/latest
-sudo dpkg --install /tmp/insomnia.deb
+# insomnia
+curl -L -o /tmp/insomnia.deb https://builds.insomnia.rest/downloads/ubuntu/latest
+sudo dpkg -i /tmp/insomnia.deb
 rm -f /tmp/insomnia.deb
 
-curl --output /tmp/gitkraken.deb --location https://release.gitkraken.com/linux/gitkraken-amd64.deb
-sudo dpkg --install /tmp/gitkraken.deb
+# gitkraken
+curl -L -o /tmp/gitkraken.deb https://release.gitkraken.com/linux/gitkraken-amd64.deb
+sudo dpkg -i /tmp/gitkraken.deb
 rm -f /tmp/gitkraken.deb
-
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
-sudo apt install -y nodejs
 
 # virtualbox
 sudo apt install -y virtualbox virtualbox-dkms virtualbox-qt virtualbox-guest-additions-iso
 
 # fonts
-sudo apt install -y fonts-droid-fallback fonts-inconsolata ttf-mscorefonts-installer
+sudo apt install -y fonts-droid-fallback fonts-inconsolata
 
 # remote
 sudo apt install -y remmina remmina-plugin-rdp remmina-plugin-vnc
