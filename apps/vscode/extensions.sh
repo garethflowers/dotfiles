5ab08c1c4a39b6394b44a42553f524ba05db0a30
Gareth Flowers <garethflowers@users.noreply.github.com>
2019-06-13 16:02:27 +0100

    feat(): script to install vscode extensions

---
+++ apps/vscode/extensions.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+code --install-extension editorconfig.editorconfig
+code --install-extension ms-vscode.vscode-typescript-tslint-plugin
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension peterjausovec.vscode-docker
+code --install-extension shanoor.vscode-nginx
+code --install-extension streetsidesoftware.code-spell-checker
