e1e802f345587b025232d553ac72e49c0e6b5152
Gareth Flowers <garethflowers@users.noreply.github.com>
2020-03-20 22:34:37 +0000

    feat(): adds hyper config

---
+++ src/.hyper.js
@@ -0,0 +1,14 @@
+// https://hyper.is#cfg
+module.exports = {
+	config: {
+		backgroundColor: 'rgb( 0, 0, 0, 0.9 )',
+		cursorColor: 'rgb( 255, 255, 255 )',
+		cursorShape: 'BEAM',
+		fontFamily: '"FiraCode-Retina", "Fira Code Retina", monospace',
+		fontSize: 14,
+		foregroundColor: 'rgb( 255, 0, 0 )',
+		lineHeight: 1.5,
+		padding: '2rem',
+		selectionColor: 'rgba( 255, 255, 255, 0.3 )',
+	},
+};
