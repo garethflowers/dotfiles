641eccb2b0a77273cd2eee1d619292836fbda36b
Gareth Flowers <garethflowers@users.noreply.github.com>
2020-05-20 22:09:53 +0100

    feat(terminal): change foreground colour

--- src/.hyper.js
+++ src/.hyper.js
@@ -6,7 +6,7 @@ module.exports = {
 		cursorShape: 'BEAM',
 		fontFamily: '"FiraCode-Retina", "Fira Code Retina", Menlo, DejaVu Sans Mono, Lucida Console, monospace',
 		fontSize: 14,
-		foregroundColor: 'rgb( 255, 0, 0 )',
+		foregroundColor: 'rgb( 200, 200, 200 )',
 		lineHeight: 1.5,
 		padding: '2rem',
 		selectionColor: 'rgba( 255, 255, 255, 0.3 )',
