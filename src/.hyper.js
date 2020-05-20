a25a36f32eb5179fd83518873a6ff80802ac95a3
Gareth Flowers <garethflowers@users.noreply.github.com>
2020-05-20 22:19:33 +0100

    feat(terminal): decrease line height

--- src/.hyper.js
+++ src/.hyper.js
@@ -7,7 +7,7 @@ module.exports = {
 		fontFamily: '"FiraCode-Retina", "Fira Code Retina", Menlo, DejaVu Sans Mono, Lucida Console, monospace',
 		fontSize: 14,
 		foregroundColor: 'rgb( 200, 200, 200 )',
-		lineHeight: 1.5,
+		lineHeight: 1.2,
 		padding: '2rem',
 		selectionColor: 'rgba( 255, 255, 255, 0.3 )',
 	},
